93c29893fe5e824564f37739b5d512a6ae15f34d
Marco Franke <mfranke87@icloud.com>
2024-05-30 14:16:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      test
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
+</template>
